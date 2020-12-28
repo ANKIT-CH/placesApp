de427b37c88eb306463b0b6c3b17392fcec2ef56
ankit <ach1134544@gmail.com>
2020-12-28 11:08:03 +0530

    added google map dependencies and other setting in android and ios folder to use google maps

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    
+    GMSServices.provideAPIKey("YOUR GOOGLE API KEY HERE")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
